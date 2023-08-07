926dd083b2e363d6c3046de83db95afc3c6b93de
Florent SANIKA <flo.sanika@gmail.com>
2023-08-08 00:41:56 +0200

    [problem_solving] flake8 compliance

--- oc_lettings_site/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# from oc_lettings_site.models import Letting
-# from oc_lettings_site.models import Address
-
-
-# admin.site.register(Letting)
-# admin.site.register(Address)
